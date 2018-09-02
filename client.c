7dbee97926929183e22e5d9f9f8863404b6460e8
poojay1 <poojay25101996@gmail.com>
2018-09-02 14:48:03 +0530

    Image transfer successful

--- client.c
+++ client.c
@@ -8,63 +8,15 @@
 #include <netinet/in.h>
 #include <string.h>
 #include <arpa/inet.h>
-#define MAXSIZE 102400
-
-
-#define SHELLSCRIPT "\
-#/bin/bash \n\
-firefox file1.html \n\
-"
-
-
-int main(int argc , char * argv[]){
-  int clientSocket, num;
+#include <stdlib.h>
+int main(){
+  int clientSocket;
   char buffer[1024];
-  char filedata[MAXSIZE];
   struct sockaddr_in serverAddr;
   socklen_t addr_size;
-
-  char cats ='0' ,cars = '0',dogs = '0',trucks = '0';
-
-  // printf("%d",strlen(argv[1]));
-
-  int len = strlen(argv[1]);
-
-  int i = 0;
-
-  while(i<len)
-  {
-     if(argv[1][i]-'0'>=0 && argv[1][i]-'0'<=4)
-     {
-         char c = argv[1][i];
-         i+=2;
-         if(argv[1][i]=='t')
-         {
-            trucks = c;
-         }
-         if(argv[1][i]=='d')
-         {
-            dogs = c;
-         }
-         if(argv[1][i]=='c')
-         {
-           i+=2;
-           if(argv[1][i]=='t')
-           cats = c;
-           else
-           cars = c;
-         }
-
-     }
-     i++;
-  }
-
-  buffer[0] = cars;
-  buffer[1] = cats;
-  buffer[2] = dogs;
-  buffer[3] = trucks;
-
-  /*---- Create the socket. The three arguments are: ----*/
+  char buffer1[1024];
+  char file_name[50];  
+/*---- Create the socket. The three arguments are: ----*/
   /* 1) Internet domain 2) Stream socket 3) Default protocol (TCP in this case) */
   clientSocket = socket(PF_INET, SOCK_STREAM, 0);
 
@@ -75,38 +27,53 @@ int main(int argc , char * argv[]){
   serverAddr.sin_port = htons(5432);
   /* Set IP address to localhost */
   serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-   // Set all bits of the padding field to 0 
+  /* Set all bits of the padding field to 0 */
   memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);
 
   /*---- Connect the socket to the server using the address struct ----*/
   addr_size = sizeof serverAddr;
   connect(clientSocket, (struct sockaddr *) &serverAddr, addr_size);
-
-  send(clientSocket,buffer,4,0);
-
-  num = recv(clientSocket, filedata, sizeof(filedata), 0);
-  if(num<=0){
-    printf("Network Error\n");
-    exit(-1);
-  }
-  filedata[num] = '\0';
-
-  FILE *fptr;
-  fptr = fopen("file1.jpeg","w+");
-  fwrite(filedata, 1, strlen(filedata), fptr);
-  fclose(fptr);
-  printf(" Data: %s", filedata);
-
-  // system(SHELLSCRIPT);
-
+  //printf("Enter you message to send\n");
+  //scanf("%s", &buffer1[0]);
+  //send(clientSocket, buffer1, 1024, 0);
   /*---- Read the message from the server into the buffer ----*/
-  // recv(clientSocket, buffer, 1024, 0);
-
-  // buffer = "2 cars 3 dogs and 4 trucks";
-
+  //recv(clientSocket, buffer, 1024, 0);
+   
   /*---- Print the received message ----*/
 
-  // printf("Data received: %s",buffer);
+  //printf("Data received: %s\n",buffer);
+  printf("Enter file to be read\n");
+  scanf("%s",file_name);
+  send(clientSocket, file_name, 50, 0);
+  recv(clientSocket, buffer, 1024, 0);
+  printf("Data received: %s\n",buffer);
+  FILE *fp = fopen("temp.html", "w+");
+  if (fp != NULL)
+  {
+      fputs(buffer, fp);
+      fclose(fp);
+  }  
+  printf("Reading Picture Size\n");
 
+//read(new_sock, &size, sizeof(int));
+int size;
+recv(clientSocket, &size, sizeof(int), 0);
+//printf("Buffer Value %s\n",buffer);
+// = atoi(buffer);
+//Read Picture Byte Array
+printf("Reading Picture Byte Array\n");
+printf("Size: %d",size);
+char p_array[size];
+//read(new_sock, p_array, size);
+recv(clientSocket, p_array, size, 0);
+printf("Content%s",p_array);
+//Convert it Back into Picture
+printf("Converting Byte Array to Picture\n");
+FILE *image;
+image = fopen("c1.jpg", "w");
+fwrite(p_array, 1, sizeof(p_array), image);
+fclose(image);
+
+  // int status = system("firefox temp.html");
   return 0;
 }
--- server.c
+++ server.c
@@ -7,13 +7,15 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
-#define MAXSIZE 102400
+#include <string.h>
+
+void lower_string(char []);
 
 
 int main(){
-  int welcomeSocket, newSocket, num;
-  char buffer[1024];
-  char filedata[MAXSIZE];
+  int welcomeSocket, newSocket;
+  char buffer[50];
+  char ch,retbuf[1024]; 
   struct sockaddr_in serverAddr;
   struct sockaddr_storage serverStorage;
   socklen_t addr_size;
@@ -34,50 +36,75 @@ int main(){
 
   /*---- Bind the address struct to the socket ----*/
   bind(welcomeSocket, (struct sockaddr *) &serverAddr, sizeof(serverAddr));
+
+  /*---- Listen on the socket, with 5 max connection requests queued ----*/
+  while(1){
   if(listen(welcomeSocket,5)==0)
     printf("I'm listening\n");
   else
     printf("Error\n");
 
+  /*---- Accept call creates a new socket for the incoming connection ----*/
+  addr_size = sizeof serverStorage;
+  newSocket = accept(welcomeSocket, (struct sockaddr *) &serverStorage, &addr_size);
 
-  while(1){
-
-    /*---- Accept call creates a new socket for the incoming connection ----*/
-    addr_size = sizeof serverStorage;
-    newSocket = accept(welcomeSocket, (struct sockaddr *) &serverStorage, &addr_size);
-
-    while(1){
-      // /*---- Listen on the socket, with 5 max connection requests queued ----*/
-      num= recv(newSocket, buffer, 4,0);
-      if(num<=0){
-        printf("Network Error\n");
-        break;
-      }
-      buffer[num] = '\0';
-      printf("Message Recevied: %s", buffer );
-
-
-      /*---- Send message to the socket of the incoming connection ----*/
-      // strcpy(buffer,"Hello World\n");
-
-      FILE *fd = fopen("Car1.jpeg", "rb");
-      fread(filedata, 1, MAXSIZE, fd);
-      fclose(fd);
-      printf(" Data being sent: %s", filedata);
-
-      if (( send(newSocket, filedata, strlen(filedata), 0) )== -1) {
-          fprintf(stderr, "Failure Sending Message\n");
-          close(newSocket);
-          exit(-1);
-      }
-      else {
-          printf("Message being sent: %s\n",buffer);
-      }
-
-    }
-    close(newSocket);
+  /*---- Send message to the socket of the incoming connection ----*/
+  recv(newSocket,buffer,50,0);
+  FILE *fp;
+  fp = fopen(buffer, "r"); // read mode
+ 
+  if (fp == NULL)
+  {
+     //perror("Error while opening the file.\n");
+     //exit(EXIT_FAILURE);
+     send(newSocket, "NO SUCH FILE EXISTS",19,0);
+  }else{
+  int i=0;
+  while((ch = fgetc(fp)) != EOF){
+      retbuf[i] = ch;
+      i = i+1;
   }
-  close(welcomeSocket);
-
-  return 0;
+  fclose(fp);
+  send(newSocket,retbuf,1024,0);
+  //strcpy(buffer,"Hello World\n");
+  //lower_string(buffer);
+  //send(newSocket,buffer,1024,0);
+  }
+  
+printf("Getting Picture Size\n");
+FILE *picture;
+picture = fopen("pic.jpg", "r");
+int size;
+fseek(picture, 0, SEEK_END);
+size = ftell(picture);
+fseek(picture, 0, SEEK_SET);
+//Send Picture Size
+printf("Sending Picture Size\n");
+printf("PICTURE SIZE %d",size);
+//write(sock, &size, sizeof(size));
+send(newSocket,&size,sizeof(size),0);
+//Send Picture as Byte Array
+printf("Sending Picture as Byte Array\n");
+char send_buffer[size];
+while(!feof(picture)) {
+    fread(send_buffer, 1, sizeof(send_buffer), picture);
+    printf("%s",send_buffer);
+    //write(sock, send_buffer, sizeof(send_buffer));
+    send(newSocket,send_buffer, sizeof(send_buffer),0);
+    bzero(send_buffer, sizeof(send_buffer));
 }
+  }
+  return 0;
+  
+}
+void lower_string(char s[]) {
+   int c = 0;
+ 
+   while (s[c] != '\0') {
+      if (s[c] >= 'A' && s[c] <= 'Z') {
+         s[c] = s[c] + 32;
+      }
+      c++;
+   }
+}
+
