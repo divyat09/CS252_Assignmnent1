a0f9d95f3e6d87071c8660b3b29b24d87f5c1e05
prithvi000 <prithvirajchavan33@gmail.com>
2018-10-28 20:54:38 +0530

    remving bcoz of api key

--- Asgn3/src/services/places.services.ts
+++
@@ -1,9 +0,0 @@
-export class PlacesService{
-    private places : {title : string}[] = [];
-    addplace(newplace: {title:string}){
-        this.places.push(newplace);
-    }
-    getplaces(){
-        return this.places.slice();
-    }
-}
